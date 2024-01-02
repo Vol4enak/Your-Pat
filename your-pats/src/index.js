6f75f408ed74e9d8c17f2acd9e42c488e2ffeafc
Vol4enak <king5vadim@gmail.com>
2024-01-02 20:43:04 +0200

    add imgs and structure

--- your-pats/src/index.js
+++ your-pats/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { App } from 'components/App';
 import './index.css';
-import App from './App';
+import { store, persistor } from 'redux/store';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store} >
+      <PersistGate loading={null}  persistor={persistor}/>
+      <BrowserRouter basename="/team-project-2">
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
-
--- your-pats/src/service/workTimeHelper/workTimeHelper.jsx
+++
@@ -1,8 +0,0 @@
-const days = ['MN', 'TU', 'WE', 'TH', 'FR', 'SA', 'SU'];
-
-export const takeWorkHours = (workDays, workArr) => {
-  workDays.map(({ isOpen, from, to }, index) => {
-
-    return isOpen ? workArr.push({day: days[index], hours: `${from}-${to}`}) : null;
-  });
-};
